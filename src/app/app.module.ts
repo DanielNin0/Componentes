7cdb4f0495b6d837f373a7b1a1eb5e9ab498adf3
Daniel Niño <hector.nino@unach.mx>
2023-01-13 12:15:50 -0600

    Entrega-Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './component/toolbar/toolbar.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { PlaceComponent } from './component/place/place.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    PlaceComponent
   ],
   imports: [
     BrowserModule
